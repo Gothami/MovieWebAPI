8e36c9d17672d63fd7cd6273d03e3e01945e1f42
gothami <gothamisa92@gmsil.com>
2019-09-14 00:41:20 +0930

    Add project files.

---
+++ MovieWebAPI/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TicketBookingDataLibrary.BusinessLogic;
+using TicketBookingDataLibrary.DataAccess;
+using TicketBookingDataLibrary.Models;
+
+namespace MovieWebAPI.Controllers
+{
+    public class MovieController : ApiController
+    {
+        public IEnumerable<MovieModel> Get()
+        {
+            return MovieProcessor.RetrieveData();
+
+        }
+    }
+}
